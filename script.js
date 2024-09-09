374aac3048ac894eeb6abcea049d752ca61190cd
ilaha2002 <ilaha_simmi_i@inbox.ru>
2024-09-09 16:29:44 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,137 @@
+const number = document.getElementById("number7");
+let firstNumber = document.getElementById("firstNumber");
+let number1 = document.getElementById("number1");
+let number2 = document.getElementById("number2");
+let number3 = document.getElementById("number3");
+let number4 = document.getElementById("number4");
+let number5 = document.getElementById("number5");
+let number6 = document.getElementById("number6");
+let number7 = document.getElementById("number7");
+let number8 = document.getElementById("number8");
+let number9 = document.getElementById("number9");
+let count = 0;
+let button
+
+
+function counter1() {
+    
+    count += Number(number1.innerText);
+    console.log(count)
+    sum.textContent = count
+    console.log(sum.textContent)
+    return;
+}
+function counter2() {
+
+    count += Number(number2.innerText);
+    console.log(count)
+    sum.textContent = count
+    console.log(sum.textContent)
+    return;
+}
+function counter3() {
+
+    count += Number(number3.innerText);
+    console.log(count)
+    sum.textContent = count
+    console.log(sum.textContent)
+    return;
+}
+function counter4() {
+
+    count += Number(number4.innerText);
+    console.log(count)
+    sum.textContent = count
+    console.log(sum.textContent)
+    return;
+}
+function counter5() {
+
+    count += Number(number5.innerText);
+    console.log(count)
+    sum.textContent = count
+    console.log(sum.textContent)
+    return;
+}
+function counter6() {
+
+    count += Number(number6.innerText);
+    console.log(count)
+    sum.textContent = count
+    console.log(sum.textContent)
+    return;
+}
+ function counter7() {
+
+    count += Number(number7.innerText);
+    console.log(count)
+    sum.textContent = count
+    console.log(sum.textContent)
+    return;
+}
+function counter8() {
+
+    count += Number(number8.innerText);
+    console.log(count)
+    sum.textContent = count
+    console.log(sum.textContent)
+    return;
+}
+function counter9() {
+
+    count += Number(number9.innerText);
+    console.log(count)
+    sum.textContent = count
+    console.log(sum.textContent)
+    return;
+}
+
+
+// if (count.length = 1) {
+//     firstNumber = Number(number9.innerText);
+//     return;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector('.buttons').onclick = (event) => {
+    //когда нажата не кнопка, а промежуток между кнопками
+    // if(!event.target.classList.contains('btn'))
+    // return;
+    //нажата кнопка AC
+    // if(event.target.classList.contains('ac'))
+    // return;
+    
+    // out.textContent = '';
+    
+    // if(event.target.classList.contains('plus-minus')) {
+    //     a = -1 * a; 
+        //Если нажата кнопка с классом plus-minus, то выполняется условие.
+    // } else if(event.target.classList.contains('percent')) {
+    //     a = a / 100; 
+        //Более чем уверена, что реализация процента калькулятора тоже пригодится :)
+    // }
+    
+    // out.textContent = a; //Запись и вывод результата на экран.
+    
+    //получаю нажатую кнопку
+    // const key = event.target.textContent;
+
+
+//нажата кнопка смены знака (+/-)
+// function plusMin() {
+//     a = -1 * a; 
+//  }
+//  out.textContent = a;
+//  document.querySelector('.plus-minus').onclick = plusMin;
+//  console.log(a);
